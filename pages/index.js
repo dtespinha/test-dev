b7c0a9e5ca33ddb25162550830c1c9935b39807a
Daniel Espinha <dtespinha@users.noreply.github.com>
2025-11-11 19:01:04 +0000

    Under construction page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function Home() {
-  return <h1>Test Dev</h1>;
+  return (
+    <main>
+      <h1>
+        <span role="img" aria-label="Em construção">
+          🚧
+        </span>{" "}
+        Em construção
+      </h1>
+      <p>
+        Esta página está em desenvolvimento. Aqui teremos um espaço para
+        compartilhar notícias, novidades, experiências e assuntos diversos sobre
+        biologia e assuntos afins. Aguardem!
+      </p>
+    </main>
+  );
 }
 
 export default Home;
