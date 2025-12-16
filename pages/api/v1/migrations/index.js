ab2c6cabac3fa92f847f9a6292eefb716de6e193
Daniel Espinha <dtespinha@users.noreply.github.com>
2025-12-16 17:40:48 +0000

    adds `/api/v1/migrations` endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,38 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  console.log(process.env.NODE_ENV);
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  } else if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    } else {
+      return response.status(200).json(migratedMigrations);
+    }
+  } else {
+    return response.status(405);
+  }
+}
+
+export default migrations;
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,45 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("DROP schema public cascade; CREATE schema public;");
+}
+
+test("POST to /api/vi/migrations should return as expected", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(201);
+  const responseBody = await response.json();
+  expect(Array.isArray(responseBody)).toBe(true);
+
+  expect(responseBody[0].path).toBe(
+    "infra/migrations/1765467921936_test-migration.js",
+  );
+  expect(responseBody[0].name).toBe("1765467921936_test-migration");
+
+  let result = await database.query("SELECT count(*) FROM pgmigrations;");
+  expect(parseInt(result.rows[0].count)).toBe(1);
+  result = await database.query("SELECT * FROM pgmigrations;");
+  expect(result.rows[0].name).toBe("1765467921936_test-migration");
+});
+
+test("POST to /api/vi/migrations and no migration to be run", async () => {
+  let response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+
+  response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody).toStrictEqual(new Array());
+
+  let result = await database.query("SELECT count(*) FROM pgmigrations;");
+  expect(parseInt(result.rows[0].count)).toBe(1);
+  result = await database.query("SELECT * FROM pgmigrations;");
+  expect(result.rows[0].name).toBe("1765467921936_test-migration");
+});
