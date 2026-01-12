3824875b18042062e7e144ad5a361731982d84da
Daniel Espinha <dtespinha@users.noreply.github.com>
2026-01-12 20:52:58 +0000

    refactor: create `models.js` file with bussines logic of migration endpoint

---
+++ models/migrator.js
@@ -0,0 +1,64 @@
+import database from "infra/database.js";
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import { ServiceError } from "../infra/errors";
+
+const migrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migrations = await migrationRunner({
+      ...migrationOptions,
+      dbClient: dbClient,
+    });
+
+    return migrations;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      cause: error,
+      message: "Error in database connection or query",
+    });
+    throw serviceErrorObject;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migrations = await migrationRunner({
+      ...migrationOptions,
+      dbClient: dbClient,
+      dryRun: false,
+    });
+
+    return migrations;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      cause: error,
+      message: "Error in database connection or query",
+    });
+    throw serviceErrorObject;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
